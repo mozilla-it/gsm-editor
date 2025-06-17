870fc9330c8b927e01a622b0494b21b6618f3f08
Dustin Lactin <dlactin@mozilla.com>
2025-06-17 10:30:39 -0600

    feat: added preview to environmente choices list

--- gsm.py
+++ gsm.py
@@ -50,7 +50,7 @@ def add_default_arguments(parser: ArgumentParser) -> None:
         "-e",
         "--env",
         type=str,
-        choices=["qa", "dev", "stage", "prod", "test"],
+        choices=["qa", "dev", "stage", "prod", "test", "preview"],
         required=True,
         help="secret env",
     )
